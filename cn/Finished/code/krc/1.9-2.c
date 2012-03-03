f6689805ef8b0f1d73fc7f996e558cc47c80447c
Wang Dingwei <gastlygem@gmail.com>
2012-03-03 08:43:32 +0800

    removed code files from Finished

--- cn/Finished/code/krc/1.9-2.c
+++
@@ -1,9 +0,0 @@
-/* bad use of while loop with compound if-statement */
-while ((len = getline(line, MAXLINE)) > 0)
-    if (len > max) {
-        max = len;
-        copy(longest, line);
-    }
-if (max > 0) /* there was a line */
-    printf("%s", longest);
-
