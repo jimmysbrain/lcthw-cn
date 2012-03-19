f1d91aaba555e5c9a5aeb8e551e02f7cef24086e
Wang Dingwei <gastlygem@gmail.com>
2012-03-19 11:32:56 +0800

    update zed's commits

--- cn/Finished/clean_p.py
+++ cn/Finished/clean_p.py
@@ -1,30 +1,40 @@
+# A dirty script to produce readable Chinese html pages.
+
+import os
+import shutil
+import glob
+
 import lxml.html
 from lxml import etree
 
-html = open("1.html").read().decode('utf8').encode('utf8')
+for htfile in glob.iglob("learn*.html"):
+    html = open(htfile).read().decode('utf8').encode('utf8')
 
-doc = lxml.html.document_fromstring(html)
+    doc = lxml.html.document_fromstring(html)
 
-content = doc.find("body").find("div").find("div").findall("div")[1].find("div")
+    content = doc.find("body").find("div").find("div").findall("div")[1].find("div")
 
-for para in content.findall("p"):
-    para.text = para.text.replace("\n", "")
+    # Remove extra line breaks from <p> tags.
+    for para in content.findall("p"):
+        para.text = para.text.replace("\n", "")
 
-for srcdiv in content.findall("div"):
-    _cnt = srcdiv.find("div")
-    if _cnt is not None:
-        cnt = _cnt.findall("span")[1]
-    	cnt.text = cnt.text.replace("\n", "")
+    # Remove extra line breaks from source code captions.
+    for srcdiv in content.findall("div"):
+        _cnt = srcdiv.find("div")
+        if _cnt is not None:
+            cnt = _cnt.findall("span")[1]
+            cnt.text = cnt.text.replace("\n", "")
 
-h2 = content.find("h2")
-if h2:
-    it = list(h2.itertext())[1]
-    head = doc.find("head").find("title")
-    head.text = it
+    # Correct page title.
+    h2 = content.find("h2")
+    if h2:
+        it = list(h2.itertext())[1]
+        head = doc.find("head").find("title")
+        head.text = it
 
-result = etree.tostring(doc,
-                        pretty_print=True,
-                        method='html',encoding="utf8")
+    result = etree.tostring(doc,
+                            pretty_print=True,
+                            method='html',encoding="utf8")
 
-print result
-
+    with open(htfile, 'w') as r:
+        r.write(result)
