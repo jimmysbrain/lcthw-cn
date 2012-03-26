b570993360460beecac9e74a26b067662e5df5b5
Wang Dingwei <gastlygem@gmail.com>
2012-03-26 10:10:11 +0800

    cleans clean.py

--- cn/Finished/clean.py
+++ cn/Finished/clean.py
@@ -1,5 +1,5 @@
+# -*- encoding: utf8 -*-
 # A dirty script to produce readable Chinese html pages.
-# TODO: s/([Chinese])([A-Za-z0-9])/\1 \2/
 
 import os
 import glob
@@ -7,9 +7,18 @@ import re
 
 from lxml import etree
 
-mix = re.compile(u"([一-龥])([A-Za-z0-9])")
 parser = etree.HTMLParser()
 prefix = "learn-c-the-hard-way"
+tags = ["dd", "li", "p", "div", "span"]
+
+def spacify(text):
+    """Makes spaces appear in the proper positions. """
+    cn_en = re.compile(u"([一-龥])([A-Za-z0-9])")
+    en_cn = re.compile(u"([A-Za-z0-9])([一-龥])")
+    cn_brk = re.compile(u"([一-龥])(\n\s*)([一-龥])")
+    text_brk = cn_brk.sub(r"\1\3", text)
+    return en_cn.sub("\1 \2", cn_en.sub("\1 \2", text_brk))
+
 
 for htfile in glob.iglob(prefix + "*.html"):
     print "Cleaning", htfile
@@ -17,28 +26,16 @@ for htfile in glob.iglob(prefix + "*.html"):
 
     content = doc.xpath('/html/body/div/div[1]/div[2]/div')[0]
 
-    # Remove extra line breaks from <p> tags.
-    for para in content.xpath("//p"):
-        if para.text is not None:
-            para.text = re.sub("\n\s*", "", para.text)
-            para.text = mix.sub("\1 \2", para.text)
-        if para.tail is not None:
-            para.tail = re.sub("\n\s*", "", para.tail)
-            para.tail = mix.sub("\1 \2", para.tail)
-
-    # Remove extra line breaks from <dd> tags.
-    for dd in content.xpath("//dl/dd"):
-        if dd.text is not None:
-            dd.text = re.sub("\n\s*", "", dd.text)
-            dd.text = mix.sub("\1 \2", dd.text)
-        if dd.tail is not None:
-            dd.tail = re.sub("\n\s*", "", dd.tail)
-            dd.tail = mix.sub("\1 \2", dd.tail)
+    for t in tags:
+        for part in content.xpath("//" + t):
+            if part.text is not None:
+                part.text = spacify(part.text)
+            if part.tail is not None:
+                part.tail = spacify(part.tail)
 
     # Remove extra line breaks from source code captions.
-    for cap in content.xpath("div/div[1]/span[2]"):
-        cap.text = re.sub("\n\s*", "", cap.text)
-        cap.text = mix.sub("\1 \2", cap.text)
+    # for cap in content.xpath("div/div[1]/span[2]"):
+    #     cap.text = spacify(cap.text)
 
     # Correct page title.
     if prefix + "ch" in htfile:  # chapters
