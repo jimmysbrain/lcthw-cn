14f70bb959b8400e5a695fec05c0d9b9d0cf4246
Zed A. Shaw <zedshaw@zedshaw.com>
2012-04-28 18:39:22 -0700

    Ex47 draft done.

--- code/liblcthw/src/lcthw/tstree.c
+++ code/liblcthw/src/lcthw/tstree.c
@@ -7,7 +7,7 @@
 static inline TSTree *TSTree_insert_base(TSTree *root, TSTree *node,
         const char *key, size_t len, void *value)
 {
-    if (node == NULL) { 
+    if(node == NULL) { 
         node = (TSTree *) calloc(1, sizeof(TSTree));
 
         if(root == NULL) {
@@ -17,10 +17,10 @@ static inline TSTree *TSTree_insert_base(TSTree *root, TSTree *node,
         node->splitchar = *key; 
     }
 
-    if (*key < node->splitchar) {
+    if(*key < node->splitchar) {
         node->low = TSTree_insert_base(root, node->low, key, len, value); 
-    } else if (*key == node->splitchar) { 
-        if (len > 1) {
+    } else if(*key == node->splitchar) { 
+        if(len > 1) {
             node->equal = TSTree_insert_base(root, node->equal, key+1, len - 1, value);
         } else {
             assert(node->value == NULL && "Duplicate insert into tst.");
@@ -44,9 +44,9 @@ void *TSTree_search(TSTree *root, const char *key, size_t len)
     size_t i = 0;
 
     while(i < len && node) {
-        if (key[i] < node->splitchar) {
+        if(key[i] < node->splitchar) {
             node = node->low; 
-        } else if (key[i] == node->splitchar) {
+        } else if(key[i] == node->splitchar) {
             i++;
             if(i < len) node = node->equal; 
         } else {
@@ -70,9 +70,9 @@ void *TSTree_search_prefix(TSTree *root, const char *key, size_t len)
     size_t i = 0;
 
     while(i < len && node) {
-        if (key[i] < node->splitchar) {
+        if(key[i] < node->splitchar) {
             node = node->low; 
-        } else if (key[i] == node->splitchar) {
+        } else if(key[i] == node->splitchar) {
             i++;
             if(i < len) {
                 if(node->value) last = node;
@@ -96,11 +96,11 @@ void *TSTree_search_prefix(TSTree *root, const char *key, size_t len)
 
 void TSTree_traverse(TSTree *node, TSTree_traverse_cb cb, void *data)
 {
-    if (!node) return;
+    if(!node) return;
 
     if(node->low) TSTree_traverse(node->low, cb, data);
 
-    if (node->equal) {
+    if(node->equal) {
         TSTree_traverse(node->equal, cb, data); 
     }
 
@@ -111,13 +111,15 @@ void TSTree_traverse(TSTree *node, TSTree_traverse_cb cb, void *data)
 
 void TSTree_destroy(TSTree *node)
 {
+    if(node == NULL) return;
+
     if(node->low) TSTree_destroy(node->low);
 
-    if (node->equal) {
+    if(node->equal) {
         TSTree_destroy(node->equal);
     }
 
     if(node->high) TSTree_destroy(node->high);
 
-    if(node) free(node);
+    free(node);
 }
